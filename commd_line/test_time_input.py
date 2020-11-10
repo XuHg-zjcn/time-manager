64bd5b5aadba79a5ec54d37f833f7b872451ca62
Xu <1687701765@qq.com>
2020-11-11 07:30:24 +0800

    add more time_input test

--- commd_line/test_time_input.py
+++ commd_line/test_time_input.py
@@ -8,7 +8,8 @@ Created on Wed Nov 11 05:03:38 2020
 from time_input import Time_input
 
 test_ok = ['n', 'now', 'n+1.1h', '+1m', '-1.0s', '+1d', 'n+.5h', 'n+0.1s']
-test_err = ['+x', 'n+n', 'nx', 'abc', 'n+1hh', 'n+1hm', 'n+hh', ' ', '']
+test_err = ['+x', 'n+n', 'nx', 'abc', 'n+1hh', 'n+1hm',
+            'n+1h5h', 'n+1h5', 'n+1x0h', ' ', '', '+']
 
 ti = Time_input()
 
