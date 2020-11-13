4dd8f5bd7d637e519a1e86bb68102e5b015fc735
Xu <1687701765@qq.com>
2020-11-13 21:38:15 +0800

    feat: wtmp python_api

--- data_collector/wtmp_cpp/print.cpp
+++ data_collector/wtmp_cpp/print.cpp
@@ -5,14 +5,12 @@
 #include <utmp.h>
 int main(int argc, char *argv[]){
     reader r = reader();
-    int type = 0;
     int n = 0;
-    long t_sta;
-    long t_end;
+    long t_sta, t_end;
     char *str_time = new char[32];
     const char *time_fmt = "%Y-%m-%d %H:%M:%S";
     while(true){
-        type = r.next(t_sta, t_end);
+        int type = r.next(t_sta, t_end);
         if(type == -1)
             break;
         cout << setw(2) << type << "  ";
