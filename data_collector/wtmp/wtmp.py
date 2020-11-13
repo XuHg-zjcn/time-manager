d192cbf73952813fcce59ef79867ba61fd29f156
Xu <1687701765@qq.com>
2020-11-13 21:51:28 +0800

    rename wtmp_cpp to wtmp, add wtmp.py iter

---
+++ data_collector/wtmp/wtmp.py
@@ -0,0 +1,16 @@
+#!/bin/python
+from build import libwtmp_cpp as wtmp
+
+class wtmp_iter:
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        sta, end, xtype = wtmp.next()
+        if xtype == -1:
+            raise StopIteration
+        return sta, end, xtype
+
+if __name__ == '__main__':
+    for sta, end, xtype in wtmp_iter():
+        print(sta, end, xtype)
