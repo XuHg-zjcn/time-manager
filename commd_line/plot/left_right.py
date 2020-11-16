5c7f7ac449ff6e4580e1829a4506f6bb07cdce0a
Xu <1687701765@qq.com>
2020-11-16 20:38:44 +0800

    plot.left_right.py set private, add docstring

--- commd_line/plot/left_right.py
+++ commd_line/plot/left_right.py
@@ -10,7 +10,19 @@ left_black = ' ▏▎▍▌▋▊▉█'
 
 
 class Syncer:
+    """
+    IntervalTree's range: range to color
+    IntervalTree's data: color name, see _color1
+    """
+
     def __init__(self, ivtree, defalut_color='r', space_color='k', end=96*8):
+        """
+        Init Syncer.
+
+        ivtree: The IntervalTree object
+        defalut_color: use to Intervals without data
+        space_color: color for IntervalTree not overlap ranges
+        """
         if ivtree.begin() < 0:
             raise ValueError('IntervalTree begin{} < 0'
                              .format(ivtree.begin()))
@@ -18,17 +30,17 @@ class Syncer:
             raise ValueError('IntervalTree end{} > fill end{}'
                              .format(ivtree.end(), end))
         self.tree = ivtree
-        self.apply_defalut_color(defalut_color)
-        self.insert_space(space_color, end)
-        self.out_str = ''          # len(out_str) include contral chara
-        self.outn = 0              # blocks in out_str
+        self._apply_defalut_color(defalut_color)
+        self._insert_space(space_color, end)
+        self._out_str = ''          # len(out_str) include contral chara
+        self._outn = 0              # blocks in out_str
 
-    def apply_defalut_color(self, defalut_color):
+    def _apply_defalut_color(self, defalut_color):
         for iv in self.tree:
             if iv.data is None:
                 iv.data = defalut_color
 
-    def insert_space(self, space_color, end):
+    def _insert_space(self, space_color, end):
         iv0 = Interval(0, self.tree.begin(), space_color)
         for iv1 in sorted(self.tree):
             if iv1.begin != iv0.end:
@@ -38,8 +50,9 @@ class Syncer:
             self.tree[iv0.end:end] = space_color
 
     def fill(self):
+        """Fill IntervalTree to output string."""
         while True:
-            o8 = self.outn*8
+            o8 = self._outn*8
             in8 = self.tree[o8:o8+8]
             if len(in8) == 0:
                 break
@@ -48,44 +61,60 @@ class Syncer:
                 n = (in8.end - o8)//8
                 if n == 0:
                     break
-                self.color1(n, in8.data)
+                self._color1(n, in8.data)
             elif len(in8) == 2:
                 in8 = sorted(in8)
                 assert in8[0].end == in8[1].begin
-                spt = in8[0].end%8
-                self.color2(spt, in8[0].data, in8[1].data)
+                spt = in8[0].end % 8
+                self._color2(spt, in8[0].data, in8[1].data)
             else:
                 break
 
-    def color2(self, spt, c1, c2):
-        assert 0 < spt <= 8
+    def _color2(self, spt, c1, c2):
+        """
+        Append a block two color to output string.
+
+        spt: spilt point, 0<x<8, chooise unicode
+        c1: left side color, see _color1
+        c2: right side color
+        """
+        assert 0 < spt < 8
         assert 0 <= len(c1) <= 1
         assert 0 <= len(c2) <= 1
         stycd = style_code(c1.upper()+c2.lower())
         unicode = left_black[spt]
-        self.outn += 1
-        self.out_str += stycd+unicode+fend
+        self._outn += 1
+        self._out_str += stycd+unicode+fend
 
-    def color1(self, n, c):
+    def _color1(self, n, c):
+        """
+        Append blocks same color to output string.
+
+        n: number of blocks
+        c: color of blocks, color name char, allow above:
+        'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w'
+        """
         assert n > 0
         stycd = style_code(c.upper())
         unicode = '█' * n
-        self.outn += n
-        self.out_str += stycd+unicode
+        self._outn += n
+        self._out_str += stycd+unicode
 
-    def end(self):
-        return self.out_str + fend
+    def get(self):
+        """Get output string."""
+        return self._out_str + fend
 
 
 def sync_ivtree(*args, **kwargs):
+    """Shortcut to use Syncer."""
     syn = Syncer(*args, **kwargs)
     syn.fill()
-    return syn.end()
+    return syn.get()
 
+
+# test code
 if __name__ == '__main__':
-    #print(sync_ivtree([10, 12, 24, 25], ['r', 'w', 'k', 'c']))
     print(sync_ivtree(IvTree2([Iv2(0, 1, 'r'),
                                Iv2(1, 63, 'w'),
                                Iv2(64, 553, 'k'),
-                               Iv2(553, 768, 'c')]),
-                      'r'))
+                               Iv2(553, 768, 'c')])))
