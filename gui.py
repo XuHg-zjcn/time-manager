bcec8e91ef7e02e5a68298f769590a83eb0e41f5
Xu <1687701765@qq.com>
2020-12-09 14:20:56 +0800

    use my_datetime.sTimeRange in GUI

--- gui.py
+++ gui.py
@@ -12,42 +12,66 @@ from Qt_GUI.pyqtgraph_plot import dt2dplot
 from datetime import datetime
 from commd_line.init_config import init_config
 from sqlite_api.TODO_db import TODO_db
+from smart_strptime.my_datetime import sTimeRange
 
 conf = init_config()
 db_path = conf['init']['db_path']
 table_name = conf['init']['table_name']
 tdb = TODO_db(db_path=db_path, table_name=table_name)
 
+class QTimeRange(sTimeRange):
+    def __new__(cls, widget_begin, widget_end):
+        self = super().__new__(QTimeRange, None, None)
+        widget_begin.dateTimeChanged.connect(lambda: self.update_begin())
+        widget_end.dateTimeChanged.connect(lambda: self.update_end())
+        self.widget_begin = widget_begin
+        self.widget_end = widget_end
+        self.update_begin()
+        self.update_end()
+        return self
+
+    def update_begin(self):
+        self._begin = self.widget_begin.dateTime().toPyDateTime()
+
+    def update_end(self):
+        self._end = self.widget_end.dateTime().toPyDateTime()
+
+    def setRange(self, begin, end=None):
+        if isinstance(begin, sTimeRange):
+            self._begin = begin._begin
+            self._end = begin._end
+        else:
+            self._begin = begin
+            self._end = end
+        self.widget_begin.setDateTime(self._begin)
+        self.widget_end.setDateTime(self._end)
 
 class controller:
     def __init__(self, ui):
         self.ui = ui
+        self.start = QTimeRange(self.ui.min_sta, self.ui.max_sta)
+        self.stop = QTimeRange(self.ui.min_end, self.ui.max_end)
+        self.ui.year.valueChanged.connect(lambda: self.change_year())
+        self.ui.update_view.clicked.connect(lambda: self.update_view())
+        # set current year
         year = datetime.now().year
         self.ui.year.setValue(year)
         self.change_year()
-        self.ui.year.valueChanged.connect(lambda: self.change_year())
-        self.ui.update_view.clicked.connect(lambda: self.update_view())
 
     def change_year(self):
         year = self.ui.year.value()
-        sta = datetime(year, 1, 1)
-        end = datetime(year, 12, 31)
-        self.ui.min_sta.setDateTime(sta)
-        self.ui.max_sta.setDateTime(end)
-        self.ui.min_end.setDateTime(sta)
-        self.ui.max_end.setDateTime(end)
-        sta = sta.timestamp()
-        end = end.timestamp()
-        plans = tdb.get_aitem({'sta_time': (sta, end), 'end_time': (sta, end)})
+        tR = sTimeRange.M(year)
+        self.start.setRange(tR)
+        self.stop.setRange(tR)
+        t_range = tR.timestamp_tuple()
+        plans = tdb.get_aitem({'sta_time': t_range, 'end_time': t_range})
         ivtree = plans.get_ivtree()
         dt2p.update_ivtree(ivtree)
 
     def update_view(self):
-        sm = self.ui.min_sta.dateTime().toPyDateTime()
-        sM = self.ui.max_sta.dateTime().toPyDateTime()
-        em = self.ui.min_end.dateTime().toPyDateTime()
-        eM = self.ui.max_end.dateTime().toPyDateTime()
-        plans = tdb.get_aitem({'sta_time': (sm, sM), 'end_time': (em, eM)})
+        start = self.start.timestamp_tuple()
+        end = self.start.timestamp_tuple()
+        plans = tdb.get_aitem({'sta_time': start, 'end_time': end})
         ivtree = plans.get_ivtree()
         dt2p.update_ivtree(ivtree)
 
---
+++ smart_strptime/my_datetime.py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 15:56:59 2020
+
+@author: xrj
+some code copy from datetime module
+"""
+from numbers import Number
+from datetime import date, datetime, timedelta
+from time import struct_time
+from .datetime_str import DateTime_str
+import sys
+sys.path.append('..')
+from my_libs.ivtree2.iv2 import Iv2
+
+class sTimeDelta(timedelta):
+    def __new__(cls, days=0, seconds=0, microseconds=0, months=0,
+                milliseconds=0, minutes=0, hours=0, weeks=0, years=0, **kwargs):
+        months += years*12
+        if isinstance(days, sTimeDelta):
+            return days
+        elif isinstance(days, timedelta):  # allow use timedelta
+            seconds = days.seconds
+            microseconds = days.microseconds
+            days = days.days
+        self = super().__new__(cls, days, seconds, microseconds,
+                               milliseconds, minutes, hours, weeks, **kwargs)
+        self._months = months
+        return self
+
+    @property
+    def months(self):
+        return self._months
+
+    def __add__(self, other):
+        if isinstance(other, timedelta):
+            _months = self._months
+            if hasattr(other, '_months'):
+                _months += other._months
+            return sTimeDelta(super().__add__(other), months = _months)
+        return NotImplemented
+
+    __radd__ = __add__
+
+    def __sub__(self, other):
+        if isinstance(other, timedelta):
+            _months = self._months
+            if hasattr(other, '_months'):
+                _months -= other._months
+            return sTimeDelta(super().__sub__(other), months = _months)
+        return NotImplemented
+
+    def __rsub__(self, other):
+        if isinstance(other, timedelta):
+            return -self + other
+        return NotImplemented
+
+    def __neg__(self):
+        return sTimeDelta(-self.days,
+                          -self.seconds,
+                          -self.microseconds,
+                          -self._months)
+
+    def __pos__(self):
+        return self
+
+    def __abs__(self):
+        if self._months < 0 or (self.days < 0 and self._months == 0):
+            return -self
+        else:
+            return self
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            # for CPython compatibility, we cannot use
+            # our __class__ here, but need a real timedelta
+            return sTimeDelta(super().__mul__(other),
+                              months=self._months*other)
+        if isinstance(other, float):
+            if self._months != 0:
+                raise ValueError("sTimeDelta with months can't multipy float")
+            return sTimeDelta(super().__mul__(other))
+        return NotImplemented
+
+    __rmul__ = __mul__
+
+    def _no_months(self, func):
+        if self._months == 0:
+            return func(super())
+        else:
+            raise ValueError("delta months must 0")
+
+    def __floordiv__(self, other):
+        return self._no_months(lambda x:x.__floordiv__(other))
+
+    def __truediv__(self, other):
+        return self._no_months(lambda x:x.__truediv__(other))
+
+    def __mod__(self, other):
+        return self._no_months(lambda x:x.__mod__(other))
+
+    def __divmod__(self, other):
+        return self._no_months(lambda x:x.__divmod__(other))
+
+    def _getstate(self):
+        return (self.days, self.seconds, self.microseconds, self._months)
+
+    def as_timedelta(self, day_per_month):
+        days = self.days + self._months*day_per_month
+        return timedelta(days, self.seconds, self.microseconds)
+
+    def __eq__(self, other):
+        if isinstance(other, Number) and other == 0:
+            return not any(self._getstate())
+        else:
+            return self._getstate() == other._getstate()
+
+    def _cmp(self, other):
+        lmin = self.as_timedelta(28)
+        lmax = self.as_timedelta(31)
+        if isinstance(other, sTimeDelta):
+            rmin = other.as_timedelta(28)
+            rmax = other.as_timedelta(31)
+        elif isinstance(other, timedelta):
+            rmin = rmax = other
+        else:
+            raise TypeError("unsupport type '{}' compare with sTimeDelta"
+                            .format(type(other)))
+        if lmax<rmin:
+            value = 1
+        elif rmax<lmin:
+            value = -1
+        else:
+            value = 0
+        return value
+
+    def _cmp0(self, other):
+        """
+        allow compare with 0
+        """
+        if isinstance(other, Number):
+            if other == 0:
+                return self._cmp(timedelta(0))
+            else:
+                raise ValueError('sTimeDelta compare with number not zero')
+        else:
+            return self._cmp(other)
+
+    def __le__(self, other):
+        return self._cmp0(other) <= 0
+
+    def __lt__(self, other):
+        return self._cmp0(other) < 0
+
+    def __ge__(self, other):
+        return self._cmp0(other) >= 0
+
+    def __gt__(self, other):
+        return self._cmp0(other) > 0
+
+    def __repr__(self):
+        args = []
+        if self._months:
+            args.append("months=%d" % self._months)
+        if self.days:
+            args.append("days=%d" % self.days)
+        if self.seconds:
+            args.append("seconds=%d" % self.seconds)
+        if self.microseconds:
+            args.append("microseconds=%d" % self.microseconds)  
+        if not args:
+            args.append('0')
+        return "%s.%s(%s)" % (self.__class__.__module__,
+                              self.__class__.__qualname__,
+                              ', '.join(args))
+
+    def __str__(self):
+        ret = super().__str__()
+        if self._months:
+            def plural(n):
+                return n, abs(n) != 1 and "s" or ""
+            ret = '%d month%s, ' % plural(self._months) + ret
+        return ret
+
+class sDateTime(datetime):
+    def __new__(cls, *args, **kwargs):
+        if len(args) == 1:
+            arg = args[0]
+            if isinstance(arg, Number):
+                return super().fromtimestamp(arg)
+            elif isinstance(arg, datetime):
+                return arg
+            elif isinstance(arg, struct_time):  # not suggest use
+                return super().__new__(cls, arg.tm_year, arg.tm_mon, arg.tm_mday,
+                                       arg.tm_hour, arg.tm_min, arg.tm_sec)
+            elif isinstance(arg, str):
+                dati_str = DateTime_str(arg)
+                dati_str.process_check()
+                self = dati_str.as_datetime()
+                return self
+            else:
+                raise TypeError("unsupported type '{}' convert to sDaTi"
+                                .format(type(arg)))
+        else:
+            return super().__new__(cls, *args, **kwargs)
+
+    def _op_with_months(self, other, func):
+        if isinstance(other, timedelta):
+            tmp = func(super(), other)
+            if hasattr(other, 'months') and other.months != 0:
+                mon = func(tmp.month, other.months)
+                y_, mon = divmod(mon-1, 12)
+                d = date(tmp.year+y_, mon+1, tmp.day)
+                return sDateTime.combine(d, tmp.time())
+            else:
+                return tmp
+        return NotImplemented
+
+    def __add__(self, other):
+        return self._op_with_months(other, lambda x,y: x.__add__(y))
+
+    def __sub__(self, other):
+        return self._op_with_months(other, lambda x,y: x.__sub__(y))
+
+    def __int__(self):
+        return int(self.timestamp())
+
+    def __float__(self):
+        return self.timestamp()
+
+class sTimeRange:
+    @classmethod
+    def isOKType(cls, x):
+        return x is None or isinstance(x, datetime)
+
+    def __new__(cls, begin, end):
+        if cls.isOKType(begin) and cls.isOKType(end):
+            pass
+        elif isinstance(begin, timedelta) and isinstance(end, timedelta):
+            end = begin + end
+        elif isinstance(begin, timedelta) and isinstance(end, datetime):
+            begin = end + begin
+        else:
+            raise TypeError('unsupported sTimeRange combine')
+        if all([begin, end]) and begin > end:
+            raise ValueError('begin after end')
+        self = object.__new__(cls)
+        self._begin = begin
+        self._end = end
+        return self
+
+    @property
+    def begin(self):
+        return self._begin
+
+    @property
+    def end(self):
+        return self._end
+
+    @classmethod
+    def M(cls, year, month=None, day=None, hour=None, minute=None, second=None):
+        paras = [year, month, day, hour, minute, second, None]
+        name_paras = ['years', 'months', 'days', 'hours', 'minutes', 'seconds']
+        maxi = paras.index(None)
+        name = name_paras[maxi-1]
+        if maxi<6 and len(set(paras[maxi:]))>1:  # not same
+            raise ValueError('rangeM can only continuous None in end')
+        paras_begin = paras[:maxi]
+        while len(paras_begin) < 3:
+            paras_begin.append(1)
+        begin = sDateTime(*paras_begin)
+        d1 = sTimeDelta(**{name:1})
+        return cls(begin, begin+d1)
+
+    def get_iv(self, data=None):
+        return Iv2(float(self._begin), float(self._end), data)
+
+    def __repr__(self):
+        return "%s.%s(%s)" % (self.__class__.__module__,
+                              self.__class__.__qualname__,
+                              ", ".join(map(repr, [self._begin, self._end])))
+
+    def __str__(self):
+        return "sTimeRange:\n{}\n{}".format(str(self._begin), str(self._end))
+
+    def datetime_tuple(self):
+        return (self._begin, self._end)
+
+    def timestamp_tuple(self):
+        return (self._begin.timestamp(), self._end.timestamp())
+
+    def length(self):
+        return self._end - self._begin
