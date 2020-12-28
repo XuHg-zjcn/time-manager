34233ae2c1bb1aadd4bf3d66337c4fd1730e8469
Xu Ruijun <1687701765@qq.com>
2020-12-29 03:04:42 +0800

    feat: choose digits of microsecond output.

--- smart_strptime/MTshort.py
+++ smart_strptime/MTshort.py
@@ -12,62 +12,71 @@ from smart_strptime.basetime import UType as U
 
 
 class group:
-    def __init__(self, pad, n, ut, skip_on=False):
+    def __init__(self, ut, n, pad=' ', skip_on=False):
         """
         :para pad: padding char
         :para n: output length, if n=0, don't use padding
         :para ut: UType
         """
         self.ut = ut
+        self.n = n
         self.skip_on = skip_on
-        if ut == U.subsec:
-            self.fmt = '{{:{:s}<.{:d}f}}'.format(pad, n)
+        if ut == U.microsec:
+            self.fmt = '{:0>6d}'
+        elif ut == U.subsec:
+            self.fmt = '{{:<.{:d}f}}'.format(n)
         else:
             self.fmt = '{{:{:s}>{:d}d}}'.format(pad, n)
-        self.spaces = ' '*n    # skip print
 
     def format(self, value):
         ret = self.fmt.format(value)
         if self.ut == U.subsec:
             assert 0 <= value < 1
             ret = ret[2:]
+        elif self.ut == U.microsec:
+            ret = ret[:self.n]
         return ret
 
 
 class MTshort:
-    """Get strftime with clear same part.
+    """
+    clear same parts datetime str.
 
-    compare to last input, find biggest different part, the before part will to
-    spaces ' ', after and the part will keep.
-
-    however complete time will keep output.
-    if biggest different part is day, month will also keep.
+    compare to last input, use ' ' instead same parts, if all parts are same use '=' instead.
     """
 
     def __init__(self, fmt='? Y-? m-? d % H:%0M:%0S', init_in=None):
         """Init MTshort object.
 
-        @para fmt: strftime format string, can't change after create
+        @para fmt: strftime format string, compatibility to old format
+                   [%?].?[YmdHMSf...]
+                   %: always output
+                   ?: output if this item or bigger unit not same to last
+                   .: optional, padding char, if format code is 'f'(microsecond), must be digits(<=6) to output
+                   Ymd...: format code    # now only support YmdHMSf
         @para init_in: input prepare datetime, will compare with first input
         """
+        # TODO: add more format codes support
         char2ut = {'Y': U.Y, 'm': U.M, 'd': U.D, 'y': U.Y,
                    'H': U.h, 'M': U.m, 'S': U.s, 'z': None,
                    'a': U.D, 'A': U.D, 'b': U.M, 'B': U.M,
-                   'c': U.s, 'I': U.h, 'p': U.h, 'u': U.us}
+                   'c': U.s, 'I': U.h, 'p': U.h, 'f': U.us}
         char2len = {'Y': 4, 'm': 2, 'd': 2, 'H': 2, 'M': 2, 'S': 2, 'y': 2,
                     'z': None, 'a': None, 'A': None, 'b': None, 'B': None,
-                    'c': 24, 'I': 2, 'p': 2, 'u': 6}
+                    'c': 24, 'I': 2, 'p': 2, 'f': 6}
         self.lst = []
         fmt_i = 0  # last index of fmt append to self.lst
-        for match in re.finditer('([%?])(.?)([YmdHMSzaAbBcIpf])', fmt):
+        for match in re.finditer('([%?])(.?)([YmdHMSyzaAbBcIpf])', fmt):
             if match.start() != fmt_i:
                 self.lst.append(fmt[fmt_i:match.start()])
             skip_on = match.group(1) == '?'
             pad = match.group(2)
             char = match.group(3)
-            n = char2len[char] if pad != '' else 0
             ut = char2ut[char]
-            self.lst.append(group(pad, n, ut, skip_on))
+            n = char2len[char] if pad != '' else 0
+            if ut == U.us:
+                n = int(pad)
+            self.lst.append(group(ut, n, pad, skip_on))
             fmt_i = match.end()
         if init_in is not None:
             self.last = init_in
@@ -86,7 +95,7 @@ class MTshort:
     def _output_lst(self, n, in_x):
         ut2name = {U.Y:'year', U.M:'month', U.D:'day',
                    U.h:'hour', U.m:'minute', U.s:'second', U.us:'microsecond'}
-        first_ut = [U.Y, U.M, U.D, U.h, U.m, U.s][n]   # first unit in output
+        first_ut = [U.Y, U.M, U.D, U.h, U.m, U.s, U.us][n]   # first unit in output
         ret = ''
         for x in self.lst:
             if isinstance(x, str):
@@ -94,7 +103,8 @@ class MTshort:
             elif isinstance(x, group):
                 name = ut2name[x.ut]
                 if x.skip_on and x.ut.value < first_ut.value:
-                    ret += x.spaces
+                    char = ' ' if n != -1 else '='   # use '=' if equal to last
+                    ret += char*x.n
                 else:
                     ret += x.format(getattr(in_x, name))
         return ret
@@ -108,7 +118,7 @@ class MTshort:
 
 # tests
 if __name__ == "__main__":
-    mts = MTshort('? Y/? m/? d % H:%0M:%0S.%6u')
+    mts = MTshort('? Y/? m/? d % H:%0M:%0S.%3f')
     datis = [datetime(2020, 11, 12, 0, 49, 5),
              datetime(2020, 11, 13, 0, 49, 5),  # test day change
              datetime(2020, 11, 13, 0,  1, 5),  # test invest
