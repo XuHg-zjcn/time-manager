5617eb5a3d02d0b64e687b0dea4a48bf2e94f5bc
Xu Ruijun <1687701765@qq.com>
2021-01-06 05:33:46 +0800

    fix: my_process.py ThrRunn or ProcRunn stop method always wait for join.

--- my_libs/my_process.py
+++ my_libs/my_process.py
@@ -26,21 +26,32 @@ brk = object()
 
 
 class Runn(Base):
+    """
+    if use second of period, please call `super().before()` in rewrite before method
+    """
     objs = []
 
-    def __init__(self, inps=None, callbacks=None):
+    def __init__(self, inps=None, callbacks=None, inp2=None):
         Base.__init__(self)
         if callbacks is None:
             callbacks = []
         self.inps = inps
         self.callbacks = callbacks
+        self.inp2 = inp2
+        self.will_stop = 0  # 0: OK, 1: wait to empty, 2: stop now
         self.objs.append(self)
+
+    def before(self):
+        if isinstance(self.inp2, SemTimer):
+            self.inp2.start()
 
     def run(self):
         self.before()
-        while True:
+        while (not self.will_stop)\
+                or (hasattr(self.inp2, '_value') and self.inp2._value != 0)\
+                or (hasattr(self.inp2, 'qsize') and self.inp2.qsize() != 0):
             obj = self.inps()
-            if obj is brk:
+            if self.will_stop == 2 or obj is brk:
                 break
             ret = self.once(obj)
             if ret is not None:
@@ -48,24 +59,19 @@ class Runn(Base):
                     cb(ret)
         self.after()
 
-    def stop(self):
+    def join(self):
         pass
 
-
-class Sem2Queue:
-    def __init__(self, sem):
-        self.sem = sem
-
-    def put(self, item=None):
-        if item is brk:
-            self.sem._value = -1
-        self.sem.release()
-
-    def get(self, blocking=True, timeout=None):
-        if self.sem._value >= 0:
-            return self.sem.acquire(blocking, timeout)
-        else:
-            return brk
+    def stop(self):
+        self.will_stop = 1
+        if isinstance(self.inp2, SemTimer):
+            self.inp2.stop()
+        if hasattr(self.inp2, 'release') and self.inp2._value == 0:
+            self.will_stop = 2
+            self.inp2.release()
+        if hasattr(self.inp2, 'put'):
+            self.inp2.put(brk)
+        self.join()
 
 
 class ThrRunn(Runn, Thread):
@@ -78,28 +84,18 @@ class ThrRunn(Runn, Thread):
         callable object: call for get data
         """
         if inps == 'sem':
-            self.inp2 = Sem2Queue(Semaphore(0))
+            inp2 = Semaphore(0)
+            inps = inp2.acquire
         elif inps == 'queue':
-            self.inp2 = Queue()
+            inp2 = Queue()
+            inps = inp2.get
         elif isinstance(inps, Number):
-            self.inp2 = Sem2Queue(SemTimer(inps))
+            inp2 = SemTimer(inps)
+            inps = inp2.acquire
         else:
-            self.inp2 = None
-        if self.inp2:
-            inps = self.inp2.get
+            inp2 = None
         Thread.__init__(self)
-        Runn.__init__(self, inps, callbacks)
-
-    def before(self):
-        if hasattr(self.inp2, 'sem') and isinstance(self.inp2.sem, SemTimer):
-            self.inp2.sem.start()
-
-    def stop(self):
-        if hasattr(self.inp2, 'sem') and isinstance(self.inp2.sem, SemTimer):
-            self.inp2.sem.stop()
-        if hasattr(self.inp2, 'put'):
-            self.inp2.put(brk)
-        self.join()
+        Runn.__init__(self, inps, callbacks, inp2)
 
 
 class ProcRunn(Runn, Process):
@@ -108,27 +104,18 @@ class ProcRunn(Runn, Process):
         see class `ThrRunn`, Queue and Semaphore is already instead by multiprocessing object.
         """
         if self.inps == 'sem':
-            self.inp2 = Sem2Queue(pSemaphore(0))
+            inp2 = pSemaphore(0)
+            inps = inp2.acquire
         elif self.inps == 'queue':
-            self.inp2 = pQueue()
+            inp2 = pQueue()
+            inps = inp2.get
         elif isinstance(self.inps, Number):
-            tim = SemTimer(self.inps)
-            self.inp2 = Sem2Queue(tim)
-            tim.start()
+            inp2 = SemTimer(self.inps)
+            inps = inp2.acquire
         else:
-            self.inp2 = None
-        if self.inp2:
-            inps = self.inp2.get
+            inp2 = None
         Process.__init__(self)
-        Runn.__init__(self, inps, callbacks)
-        self.inps = inps
-
-    def stop(self):
-        if hasattr(self.inp2, 'sem') and isinstance(self.inp2.sem, SemTimer):
-            self.inp2.sem.stop()
-        if hasattr(self.inp2, 'put'):
-            self.inp2.put(brk)
-        self.join()
+        Runn.__init__(self, inps, callbacks, inp2)
 
 
 def stop_all():
