1aa19ad02185abac7c10a161c43a67cb40b56f16
Xu Ruijun <1687701765@qq.com>
2021-01-06 04:17:12 +0800

    feat: my_process.py new API

--- my_libs/my_process.py
+++ my_libs/my_process.py
@@ -1,6 +1,14 @@
+from numbers import Number
+
 from threading import Thread
+from threading import Semaphore
+from queue import Queue
+
 from multiprocessing import Process
-from multiprocessing import Value
+from multiprocessing import Semaphore as pSemaphore
+from multiprocessing import Queue as pQueue
+
+from my_libs.timer import SemTimer
 
 
 class Base:
@@ -14,22 +22,25 @@ class Base:
         pass
 
 
+brk = object()
+
+
 class Runn(Base):
+    objs = []
+
     def __init__(self, inps=None, callbacks=None):
         Base.__init__(self)
         if callbacks is None:
             callbacks = []
         self.inps = inps
         self.callbacks = callbacks
-
-    def is_break(self):
-        return False
+        self.objs.append(self)
 
     def run(self):
         self.before()
         while True:
             obj = self.inps()
-            if self.is_break():
+            if obj is brk:
                 break
             ret = self.once(obj)
             if ret is not None:
@@ -41,29 +52,85 @@ class Runn(Base):
         pass
 
 
+class Sem2Queue:
+    def __init__(self, sem):
+        self.sem = sem
+
+    def put(self, item=None):
+        if item is brk:
+            self.sem._value = -1
+        self.sem.release()
+
+    def get(self, blocking=True, timeout=None):
+        if self.sem._value >= 0:
+            return self.sem.acquire(blocking, timeout)
+        else:
+            return brk
+
+
 class ThrRunn(Runn, Thread):
-    def __init__(self, inps=None, callbacks=None):
+    def __init__(self, inps, callbacks=None):
+        """
+        :para inps: can be str 'sem' or 'queue', second of period, callable object.
+        'sem': create Sem2Queue object, `self.inp2.put()` run once, `self.inp2(brk)` stop running
+        'queue': create a Queue for input `self.inp2 = Queue()`
+        float/int: second of period
+        callable object: call for get data
+        """
+        if inps == 'sem':
+            self.inp2 = Sem2Queue(Semaphore(0))
+        elif inps == 'queue':
+            self.inp2 = Queue()
+        elif isinstance(inps, Number):
+            self.inp2 = Sem2Queue(SemTimer(inps))
+        else:
+            self.inp2 = None
+        if self.inp2:
+            inps = self.inp2.get
         Thread.__init__(self)
         Runn.__init__(self, inps, callbacks)
-        self.running = True
 
-    def is_break(self):
-        return not self.running
+    def before(self):
+        if hasattr(self.inp2, 'sem') and isinstance(self.inp2.sem, SemTimer):
+            self.inp2.sem.start()
 
     def stop(self):
-        self.running = False
+        if hasattr(self.inp2, 'sem') and isinstance(self.inp2.sem, SemTimer):
+            self.inp2.sem.stop()
+        if hasattr(self.inp2, 'put'):
+            self.inp2.put(brk)
         self.join()
 
 
 class ProcRunn(Runn, Process):
-    def __init__(self, inps=None, callbacks=None):
+    def __init__(self, inps, callbacks=None):
+        """
+        see class `ThrRunn`, Queue and Semaphore is already instead by multiprocessing object.
+        """
+        if self.inps == 'sem':
+            self.inp2 = Sem2Queue(pSemaphore(0))
+        elif self.inps == 'queue':
+            self.inp2 = pQueue()
+        elif isinstance(self.inps, Number):
+            tim = SemTimer(self.inps)
+            self.inp2 = Sem2Queue(tim)
+            tim.start()
+        else:
+            self.inp2 = None
+        if self.inp2:
+            inps = self.inp2.get
         Process.__init__(self)
         Runn.__init__(self, inps, callbacks)
-        self.running = Value('i', 1)
-
-    def is_break(self):
-        return not self.running.value
+        self.inps = inps
 
     def stop(self):
-        self.running.value = 0
+        if hasattr(self.inp2, 'sem') and isinstance(self.inp2.sem, SemTimer):
+            self.inp2.sem.stop()
+        if hasattr(self.inp2, 'put'):
+            self.inp2.put(brk)
         self.join()
+
+
+def stop_all():
+    for runn in Runn.objs[::-1]:  # revise stop
+        runn.stop()
--- processes/eyes_screen.py
+++ processes/eyes_screen.py
@@ -1,24 +1,29 @@
 from collectors.camera import Camera
-from collectors.timer import SemTimer
 from analyzers.mtcnn_face import MTCNNFace
 from analyzers.face_check import AreaChecker
 from recorders.video_recoder import VideoRecoder
 from recorders.sqlite_face import FaceDB
-from queue import Queue
+import signal
 
-tim = SemTimer(0.5)
-que_rec = Queue()
-que_mtc = Queue()
-que_fdb = Queue()
+
+def sigint(signalnum, handler):
+    cam.stop()
+    rec.stop()
+    mtc.stop()
+    fdb.stop()
+    ac.sound.stop()
+
+signal.signal(signal.SIGINT, sigint)
+
+
 ac = AreaChecker()
-cam = Camera(tim.acquire, (que_rec.put, que_mtc.put))
-rec = VideoRecoder(que_rec.get)
-mtc = MTCNNFace(que_mtc.get, (ac.once, que_fdb.put))
-fdb = FaceDB(que_fdb.get)
+rec = VideoRecoder('queue')
+fdb = FaceDB('queue')
+mtc = MTCNNFace('queue', (fdb.inp2.put, ac.once))
+cam = Camera(0.5, (rec.inp2.put, mtc.inp2.put))
 
 if __name__ == '__main__':
     cam.start()
     rec.start()
     mtc.start()
     fdb.start()
-    tim.start()
