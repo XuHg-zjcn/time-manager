24dc66349f70b53ce096b8b22f7320bbdbc68621
Xu Ruijun <1687701765@qq.com>
2021-01-05 21:54:02 +0800

    feat: add sqlite_face.py FaceDB

--- processes/eyes_screen.py
+++ processes/eyes_screen.py
@@ -3,19 +3,22 @@ from collectors.timer import SemTimer
 from analyzers.mtcnn_face import MTCNNFace
 from analyzers.face_check import AreaChecker
 from recorders.video_recoder import VideoRecoder
-from collectors.timer import SemTimer
+from recorders.sqlite_face import FaceDB
 from queue import Queue
 
 tim = SemTimer(0.5)
 que_rec = Queue()
 que_mtc = Queue()
+que_fdb = Queue()
 ac = AreaChecker()
 cam = Camera(tim.acquire, (que_rec.put, que_mtc.put))
 rec = VideoRecoder(que_rec.get)
-mtc = MTCNNFace(que_mtc.get, (ac.once,))
+mtc = MTCNNFace(que_mtc.get, (ac.once, que_fdb.put))
+fdb = FaceDB(que_fdb.get)
 
 if __name__ == '__main__':
     cam.start()
     rec.start()
     mtc.start()
+    fdb.start()
     tim.start()
--- recorders/sqlite_face.py
+++ recorders/sqlite_face.py
@@ -1,16 +1,25 @@
 import sqlite3
+from my_libs.my_process import ThrRunn
 from .record import Record
+from commd_line.init_config import init_config
+
+conf = init_config()
 
 
-class FaceDB(Record):
+class FaceDB(ThrRunn, Record):
     """
     method except __init__ can only call in same thread, else raise Error below:
 
     sqlite3.ProgrammingError: SQLite objects created in a thread can only be used in that same thread.
     The object was created in thread id 140585671317248 and this is thread id 140588434704192.
     """
-    def __init__(self, db_path, auto_commit=True):
-        super().__init__()
+    def __init__(self,
+                 inps,
+                 callbacks=None,
+                 db_path=conf['init']['db_path'],
+                 auto_commit=True):
+        ThrRunn.__init__(self, inps, callbacks)
+        Record.__init__(self)
         self._db_path = db_path
         self._conn = None
         self.auto_commit = auto_commit
@@ -24,7 +33,7 @@ class FaceDB(Record):
                'n_x, n_y, lm_x, lm_y, rm_x, rm_y, confidence)')
         cur.execute(sql)
 
-    def once(self, face):
+    def save_once(self, face):
         cur = self._conn.cursor()
         sql = ('INSERT INTO face_det '
                '(cam_id, t_cap, frame_i, x1, y1, w, h, le_x, le_y, re_x, re_y, '
@@ -38,6 +47,10 @@ class FaceDB(Record):
                           face.res_d['confidence']))
         if self.auto_commit:
             self._conn.commit()
+
+    def once(self, obj):
+        for face in obj:
+            self.save_once(face)
 
     def after(self):
         self._conn.commit()
