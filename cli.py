361defe2ada7f9a0a9e0607326632cd1b243f4a4
Xu Ruijun <1687701765@qq.com>
2021-01-16 22:21:40 +0800

    feat: cli.py '__main__' body

--- cli.py
+++ cli.py
@@ -19,6 +19,15 @@ ac = auto_collect.Collectors(conn, tdb)
 ac.run_enable()
 
 
+def input_op():
+    return input('''请输入要进行的操作:
+1. 添加任务
+2. 列出任务
+3. 收集数据
+4. 修改任务
+''')
+
+
 def add_task():
     name = input('名称:')
     sta_time = cti('开始时间:')
@@ -30,22 +39,23 @@ def add_task():
     tdb.conn.commit()
 
 
-tdb.print_doings()
-op = input('请输入要进行的操作:\n'
-           '1. 添加任务\n'
-           '2. 列出任务\n'
-           '3. 收集数据\n'
-           '4. 修改任务\n')
-if op == '1':
-    add_task()
-elif op == '2':
+def list_tasks():
     sta = cti('开始时间:')
     end = cti('结束时间:')
     plans = tdb.get_conds_plans({'sta': (sta, end), 'end': (sta, end)})
     print(plans)
     ivtree = plans.get_ivtree(lambda p: Plan(p).get_collect_color(colls))
     time2d_autosize(ivtree)
-elif op == '3':
-    ac.cli()
-else:
-    print('输入有误，请输入正确的序号1-4')
+
+
+if __name__ == '__main__':
+    tdb.print_doings()
+    op = input_op()
+    if op == '1':
+        add_task()
+    elif op == '2':
+        list_tasks()
+    elif op == '3':
+        ac.cli()
+    else:
+        print('输入有误，请输入正确的序号1-4')
