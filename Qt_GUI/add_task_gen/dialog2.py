f9ad088d0d4c694bd861243581c8620f588d77ab
Xu Ruijun <1687701765@qq.com>
2021-02-16 21:48:26 +0800

    use module croniter, CronPointGen instead DatetimePointGen

--- Qt_GUI/add_task_gen/dialog2.py
+++ Qt_GUI/add_task_gen/dialog2.py
@@ -2,6 +2,7 @@ import datetime
 
 from Qt_GUI.add_task_gen.dialog import Ui_Dialog
 from commd_line.init_config import conn
+from my_libs.smart_strptime import TimeDelta_str
 from my_libs.sqltable import onlyone_process
 from sqlite_api.task_db import TaskTable
 from sqlite_api.task_gen import TaskGenTable, TaskGen
@@ -13,53 +14,29 @@ task_tab = TaskTable(conn)
 # TODO: 3 spin box as a widget, direct get range obj.
 class AddTaskGenDialog(Ui_Dialog):
     def get_tg(self):
-        year = range(self.year_min.value(), self.year_max.value()+1, self.year_period.value())
-        mon = range(self.mon_min.value(), self.mon_max.value()+1, self.mon_period.value())
-        day = range(self.day_min.value(), self.day_max.value()+1, self.day_period.value())
-        week = range(self.week_min.value(), self.week_max.value()+1, self.week_period.value())
-        hour = range(self.hour_min.value(), self.hour_max.value()+1, self.hour_period.value())
-        minu = range(self.minu_min.value(), self.minu_max.value()+1, self.minu_period.value())
-        sec = range(self.sec_min.value(), self.sec_max.value()+1, self.sec_period.value())
-        long = datetime.timedelta(days=self.day_long.value(),
-                                  hours=self.hour_long.value(),
-                                  minutes=self.minu_long.value(),
-                                  seconds=self.sec_long.value()).total_seconds()
+        start = self.start.dateTime().toTime_t()
+        stop = self.stop.dateTime().toTime_t()
+        cron = self.cron_text.text()
+        long_text = self.long_text.text()
+        td_str = TimeDelta_str(long_text)
+        td_str.process_check()
+        long = td_str.as_sec()
         name = self.name_comb.currentText()
         rec_id = self.recid_spin.value()
         type_id = self.typeid_spin.value()
-        tg = TaskGen(name, rec_id, type_id, long,
-                     year, mon, day, week, hour, minu, sec)
+        tg = TaskGen(name, rec_id, type_id, long, cron, start, stop)
         return tg
 
     def set_tg(self, tg: TaskGen):
-        year, mon, day, hour, minu, sec = tg.groups
-        week = tg.week
-        long = tg.long
-        self.year_min.setValue(year.start)
-        self.year_max.setValue(year.stop-1)
-        self.year_period.setValue(year.step)
-        self.mon_min.setValue(mon.start)
-        self.mon_max.setValue(mon.stop-1)
-        self.mon_period.setValue(mon.step)
-        self.day_min.setValue(day.start)
-        self.day_max.setValue(day.stop-1)
-        self.day_period.setValue(day.step)
-        self.week_min.setValue(week.start)
-        self.week_max.setValue(week.stop-1)
-        self.week_period.setValue(week.step)
-        self.hour_min.setValue(hour.start)
-        self.hour_max.setValue(hour.stop-1)
-        self.hour_period.setValue(hour.step)
-        self.minu_min.setValue(minu.start)
-        self.minu_max.setValue(minu.stop-1)
-        self.minu_period.setValue(minu.step)
-        self.sec_min.setValue(sec.start)
-        self.sec_max.setValue(sec.stop-1)
-        self.sec_period.setValue(sec.step)
-        self.day_long.setValue(long//86400); long%=86400
-        self.hour_long.setValue(long//3600); long%=3600
-        self.minu_long.setValue(long//60); long%=60
-        self.sec_long.setValue(long)
+        self.start.setDateTime(datetime.datetime.fromtimestamp(tg.start_time))
+        self.stop.setDateTime(datetime.datetime.fromtimestamp(tg.stop_time))
+        self.cron_text.setText(tg._expr_format)
+        # ', ' can't pass check, raise Error below:
+        # ValueError: multiply unused char continuous
+        long_str = str(datetime.timedelta(seconds=tg.long)).replace(',', '')
+        self.long_text.setText(long_str)
+        self.recid_spin.setValue(tg.db_fields['rec_id'])
+        self.typeid_spin.setValue(tg.db_fields['type_id'])
 
     def add_db_slot(self, b):
         tg = self.get_tg()
--- sqlite_api/task_gen.py
+++ sqlite_api/task_gen.py
@@ -1,116 +1,37 @@
-import datetime
-from collections import Iterable, Container
+from croniter import croniter
 
 from my_libs.dump_table import DumpTable, DumpBaseCls
 from sqlite_api.task_db import Plan, TaskTable
 
 
-class UpBit(Exception):
-    pass  # up bigger iterators, and reinit smaller iterators
-
-
-class MyStop(Exception):
-    pass  # end of iter SeqGen
-
-
-skip = object()  # flag of skip the current result, can return in SeqGen.test
-
-
-class SeqGen:
-    def __init__(self, groups, chks=None):
-        self.groups = list(map(lambda x: x if isinstance(x, Iterable) else (x,), groups))
-        assert len(self.groups) >= 1, 'groups length must >=1'
-        self.states = list(map(iter, self.groups))
-        self.last_s = list(map(next, self.states[:-1])) + [None]  # don't call last iter
-        if chks is None:
-            chks = []
-        self.chks = chks
+class CronPointGen(croniter):
+    def __init__(self, expr_format, start_time=None, stop_time=None, max_step=1000):
+        super().__init__(expr_format, start_time)
+        self.stop_time = stop_time
+        self.max_step = max_step
         self.count = 0
 
-    def test(self):
-        try:
-            for chk in self.chks:
-                chk(self.last_s)
-        except Exception:
-            return skip
-
-    def clears(self, index):
-        """
-        reset >index iterators
-        """
-        if index < 0:
-            index = len(self.groups) + index
-        for i in range(index, len(self.groups)):    # reset iterators
-            self.states[i] = iter(self.groups[i])
-            self.last_s[i] = next(self.states[i])
-
-    def next(self, index=-1):
-        if index < -len(self.states):
-            raise MyStop                            # negative index over head
-        try:
-            self.last_s[index] = next(self.states[index])
-            while True:
-                if self.test() is not skip:
-                    break
-                self.next()
-        except (StopIteration, UpBit):
-            assert index != 0, 'index over head'
-            self.clears(index)  # notice: clear all before raise MyStop, the state will not keep.
-            self.next(index-1)
-        else:
-            self.count += 1
-            return self.last_s
-
-    def proc(self):
-        return self.last_s
-
-    def __iter__(self):
-        return self
-
     def __next__(self):
-        try:
-            self.next()
-        except MyStop:
+        ret = super().__next__()
+        self.count += 1
+        if ret > self.stop_time or self.count > self.max_step:
             raise StopIteration
-        return self.proc()
+        return ret
 
 
-class DatetimePointGen(SeqGen):
-    def __init__(self, year=range(2000, 2100),
-                 mon=range(1, 13), day=range(1, 32), week=None,
-                 hour=0, minu=0, sec=0):
-        groups = [year, mon, day, hour, minu, sec]
-        super().__init__(groups)
-        if isinstance(week, Container) or week is None:
-            pass
-        if isinstance(week, int):
-            week = (week,)
-        self.week = week
+class TaskGen(CronPointGen, DumpBaseCls):
+    names_autoload = {'id', 'table', 'name', 'rec_id', 'type_id'}
 
-    def test(self):
-        try:
-            dt = datetime.datetime(*self.last_s)
-        except ValueError as e:
-            raise UpBit(e)
-        if self.week is not None:
-            if dt.weekday() not in self.week:
-                return skip
-
-    def proc(self):
-        return datetime.datetime(*self.last_s)
-
-
-class TaskGen(DatetimePointGen, DumpBaseCls):
     def __init__(self, name='tg', rec_id=-1, type_id=-1,
                  long=3600.0, *args, **kwargs):
-        DatetimePointGen.__init__(self, *args, **kwargs)
+        CronPointGen.__init__(self, *args, **kwargs)
         DumpBaseCls.__init__(self, name)
         self.db_fields['rec_id'] = rec_id
         self.db_fields['type_id'] = type_id
         self.long = long
 
-    def proc(self):
-        sta = super().proc().timestamp()
+    def __next__(self):
+        sta = CronPointGen.__next__(self)
         end = sta + self.long
         d = {'rec_id': self.db_fields['rec_id'],
              'type_id': self.db_fields['type_id'],
