0f1d0e87271ffdb4e1d92418a7ffc6862f96d55e
Xu Ruijun <1687701765@qq.com>
2021-02-22 13:27:46 +0800

    feat: easy_api more sound input type supported

--- my_libs/easy_api.py
+++ my_libs/easy_api.py
@@ -1,14 +1,35 @@
+import math
+import numpy as np
 from playsound import playsound
+import sounddevice as sd
 
 from commd_line.init_config import conf
 from notifys.gnome import GiNotify
+from notifys.gen_sound import SoundGene
 
 gi = GiNotify()
+pi = math.pi
 
 
 def message(title, text):
     gi.once(title, text)
 
 
-def sound(name):
-    playsound(f'./sounds/{name}.wav')
+def sound(x):
+    if isinstance(x, str):
+        playsound(f'./sounds/{x}.wav')
+    elif isinstance(x, SoundGene):
+        x.once(True)
+    elif isinstance(x, np.ndarray) and x.ndim == 1:
+        if x.shape[0] < 44100*0.05:
+            s = 'short'
+        elif x.shape[0] > 44100*60:
+            s = 'long'
+        else:
+            s = ''
+        if s:
+            print(f'warning: sound too {s}, {x.shape[0]/44100}s, '
+                  'default samplerate=44100Hz')
+        sd.play(x, 44100)
+    else:
+        raise TypeError('unsupported type', type(x))
